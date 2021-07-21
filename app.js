15e08e3d19f058dc9a18a039c25e522aac3dca93
danbiilee <danbi.db@gmail.com>
2021-07-21 20:35:28 +0900

    03-04: 문자열을 활용한 HTML 다루기

--- app.js
+++ app.js
@@ -5,21 +5,18 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json"; // @id 마킹
 
-// false: 동기, true: 비동기
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
+// 중복코드 개선
+function getData(url) {
+  ajax.open("GET", url, false); // false: 동기, true: 비동기
+  ajax.send();
 
-// 2. 입력 데이터 처리
-const newsFeed = JSON.parse(ajax.response);
+  return JSON.parse(ajax.response);
+}
 
 // hashchange event -> 본 기능인 북마크 기능을 사용하지 않고 페이지 전환을 할 수 있도록 콜백함수 전달
 window.addEventListener("hashchange", function () {
   const id = location.hash.substr(1);
-
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
   const title = document.createElement("h1");
 
   title.innerHTML = newsContent.title;
@@ -27,18 +24,25 @@ window.addEventListener("hashchange", function () {
   content.appendChild(title);
 });
 
+// 2. 입력 데이터 처리
+const newsFeed = getData(NEWS_URL);
+
 // 3. 데이터 출력
 const ul = document.createElement("ul");
 for (let i = 0; i < newsFeed.length; i++) {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
+  const div = document.createElement("div"); // li, a를 담아줄 임시 부모 요소
 
-  // 제목 클릭 -> 이벤트 감지 -> 내용 확인
-  a.href = `#${newsFeed[i].id}`; // 링크에 해쉬+id 값 등록 -> hashchange 이벤트 트리거
-  a.innerHTML = `${newsFeed[i].title}(${newsFeed[i].comments_count})`;
+  // 링크에 해쉬+id 값 등록 -> hashchange 이벤트 트리거
+  div.innerHTML = `
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title}(${newsFeed[i].comments_count})
+      </a>
+    </li>
+  `;
 
-  li.appendChild(a);
-  ul.appendChild(li);
+  // ul.appendChild(div.children[0]); // 자식 요소들은 배열로 담겨있음
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
